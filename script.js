b4973f36e4bd53b003f92f4a1586ec816b1ac59e
lucaslotti05 <llotti05owo@gmail.com>
2026-08-17 01:20:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const boton = document.getElementById("botonAleatorio");
+const boton_esp = document.getElementById("botonEsp");
+const boton_anglo = document.getElementById("botonAnglo");
 const is = document.getElementById("is");
 const mocion = document.getElementById("mocion");
 const cajaIs = document.getElementById("caja-is");
@@ -7,8 +8,44 @@ const separador = document.getElementById("separador");
 const torneo = document.getElementById("caja-torneo");
 
 
-boton.addEventListener("click", function(){ //El evento es que cuando se clickee en el boton hace la funcion genérica
+boton_esp.addEventListener("click", function(){ //El evento es que cuando se clickee en el boton hace la funcion genérica
     fetch("full.csv") //pilla los datos
+        .then(function(respuesta){ //los pasa a la funcion generica, el resultado de antes es la "respuesta"
+            return respuesta.text(); //convierte el contenido a texto en plano
+        })
+        .then(function(texto){
+            const filas = texto.trim().split("\n"); //el texto quita los espacios sobrantes, el split lo separa en un array según los saltos de línea
+            // const datos = filas.slice(1); //Quita el primer elemento del array
+
+            const datos = filas; //Quita el primer elemento del array
+
+            const tabla = datos.map(function(fila) {
+                return fila.split(";");
+            });
+            
+            const indice = Math.floor(Math.random() * tabla.length);
+            const filaChosen = tabla[indice];
+
+            separador.style.display = "flex"
+
+            if (filaChosen[1] === ""){
+                cajaIs.style.display = "none";
+            } else {
+                cajaIs.style.display = "block"
+                is.innerHTML = "<p>" + filaChosen[1] + "</p>";
+            }
+
+            
+            cajaMocion.style.display = "block";
+            mocion.innerHTML = "<p>" + filaChosen[2] + "</p>";
+            torneo.style.display = "block";
+            torneo.innerHTML = "<p>" + filaChosen[0] + "</p>";
+        });
+    
+});
+
+boton_anglo.addEventListener("click", function(){ //El evento es que cuando se clickee en el boton hace la funcion genérica
+    fetch("eng.csv") //pilla los datos
         .then(function(respuesta){ //los pasa a la funcion generica, el resultado de antes es la "respuesta"
             return respuesta.text(); //convierte el contenido a texto en plano
         })
